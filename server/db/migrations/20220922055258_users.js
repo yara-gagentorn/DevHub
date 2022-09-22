670c7fc0b411c8238a5dff98164fc5ee1e12e2fd
Vanessa Gurung <vanessagur@gmail.com>
2022-09-22 22:45:09 +1200

    users db

---
+++ server/db/migrations/20220922055258_users.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id')
+    table.string('first_name')
+    table.string('last_name')
+    table.string('role')
+    table.integer('todos_id').references()
+    table.integer('resources_id').references()
+    table.integer('journal_id').references()
+    table.integer('auth0-id')
+    table.blob('profile_picture')
+    table.string('pronouns')
+    table.string('github_link')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('users')
+}
