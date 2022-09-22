903370cace69a22ab2b1e07c955543e95933dec0
Yara <yaragagen@gmail.com>
2022-09-22 17:31:59 +1200

    setup boilerplate

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const path = require('path')
 const express = require('express')
 
-const fruitRoutes = require('./routes/fruits')
-const userRoutes = require('./routes/users')
+const todosRoutes = require('./routes/todos')
+const announcementsRoutes = require('./routes/announcements')
+const resourcesRoutes = require('./routes/resources')
+
+
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/fruits', fruitRoutes)
-server.use('/api/v1/users', userRoutes)
+
+server.use('/api/v1/announcements', announcementsRoutes)
+server.use('/api/v1/todos', todosRoutes)
+server.use('/api/v1/resources', resourcesRoutes)
+server.use('/api/v1/onthefloor', onthefloorRoutes)
+server.use('/api/v1/profile', profileRoutes)
+
+
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'))
