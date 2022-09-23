4f8c7d1f13b88aee08cb1822f44ac3c75f168852
sebapian <sebapian95@gmail.com>
2022-09-23 13:15:03 +1200

    added domain to server

--- server/auth0.js
+++ server/auth0.js
@@ -2,8 +2,8 @@ const jwt = require('express-jwt')
 const jwks = require('jwks-rsa')
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://gardenz.au.auth0.com'
-const audience = 'https://fruits/api'
+const domain = 'https://pohutukawa-sebastian.au.auth0.com'
+const audience = 'https://devhub/api'
 
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
