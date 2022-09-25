9c0904f49777ef481dc709e3cd7756b523911be9
sebapian <sebapian95@gmail.com>
2022-09-26 10:14:19 +1300

    edit journals database and added seeds

--- server/db/migrations/20220922235502_journals.js
+++ server/db/migrations/20220922235502_journals.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('journals', (table) => {
     table.increments('id'),
       table.integer('user_id').references(),
-      table.string('title'),
+      table.date('date'),
       table.string('content'),
       table.string('URL_1'),
       table.string('URL_2'),
--- server/db/seeds/test-journals.js
+++ server/db/seeds/test-journals.js
@@ -6,8 +6,9 @@ exports.seed = (knex) => {
         {
           id: 1,
           user_id: 1,
-          title: 'Reflection on Agile',
-          content: 'Agile is stinky',
+          date: new Date('September 6, 2022, 12:05:00'),
+          content:
+            'Teaching went well, I got feedback from students that I explained the technical terms very well through analogies! Yay!',
           URL_1: null,
           URL_2: null,
           URL_3: null,
@@ -15,8 +16,29 @@ exports.seed = (knex) => {
         {
           id: 2,
           user_id: 2,
-          title: 'Learning plan reflection',
-          content: 'What learning plan?',
+          date: new Date('September 6, 2022, 12:05:00'),
+          content:
+            "I felt that today Alex explain testing very well! I really appreciated Alex's teaching and I hope I can be as good as a tester as him!",
+          URL_1: null,
+          URL_2: null,
+          URL_3: null,
+        },
+        {
+          id: 3,
+          user_id: 1,
+          date: new Date('September 7, 2022, 12:05:00'),
+          content:
+            "Today I called the students Full Stack Developers! I'm so happy to see how far they have come from when they first started coding :)",
+          URL_1: null,
+          URL_2: null,
+          URL_3: null,
+        },
+        {
+          id: 4,
+          user_id: 2,
+          date: new Date('September 7, 2022, 12:05:00'),
+          content:
+            "I am so happy Ahmad called us Full Stack Developers, I feel that all my effort and countless hours have paid off into something. Can't wait to see what's in stall for me!",
           URL_1: null,
           URL_2: null,
           URL_3: null,
